8ab83e5ebf5f78ca2ed014d8015f0f8fcf8cd55c
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-06 15:58:43 -0300

    updates

--- public/indexCodigo.js
+++ public/indexCodigo.js
@@ -1,4 +1,5 @@
-const botaoT = document.getElementById("troca")
+const botaoT = document.getElementById("post")
+const botaoG = document.getElementById("get")
 botaoT.addEventListener("click", async () => {
     const opcoes = {
         method: "POST",
@@ -6,6 +7,18 @@ botaoT.addEventListener("click", async () => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({id: 4, name: "item 7"})
+    }
+    const response = await fetch("/api/items",opcoes)
+        .then(responsemensagem = await response.json())
+        .then(document.getElementById("amostra").innerHTML = `${responsemensagem}`)
+    
+})
+botaoG.addEventListener("click", async () => {
+    const opcoes = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
     }
     const response = await fetch("/api/items",opcoes)
         .then(responsemensagem = await response.json())
