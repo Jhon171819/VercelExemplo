e4426473e6354fe8fe5842ab2161d162de76dab6
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-06 16:57:43 -0300

    Update

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ app.post(url + '/adi', (req, res) => {
   res.json({ resultado });
 });
 
-app.get(url + '/adi', (req,res) => res.json({ resultado }));
+app.get(url + '/adi', (req,res) => {res.json({ resultado })});
 
 app.post(url + '/mult', (req, res) => {
   const { valor1, valor2 } = req.body;
@@ -20,21 +20,22 @@ app.post(url + '/mult', (req, res) => {
   res.json({ resultado });
 });
 
-app.get(url + '/mult', (req,res) => res.json({ resultado }));
+app.get(url + '/mult', (req,res) => {res.json({ resultado })});
 
 app.post(url + '/sub', (req, res) => {
   const { valor1, valor2 } = req.body;
   const resultado = valor1 - valor2;
   res.json({ resultado });
 });
-app.get(url + '/sub', (req,res) => res.json({ resultado }));
-
+app.get(url + '/sub', (req,res) => {
+  res.json({ resultado });
+});
 app.post(url + '/divs', (req, res) => {
   const { valor1, valor2 } = req.body;
   const resultado = valor1 / valor2;
   res.json({ resultado });
 });
-app.get(url + '/divs', (req,res) => res.json({ resultado }));
+app.get(url + '/divs', (req,res) => {res.json({ resultado })});
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
