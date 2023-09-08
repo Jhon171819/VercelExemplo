dc4d1026bd124b6bc9a3c37db919c955f8e9af11
Jhon171819 <jonathancosta1717@gmail.com>
2023-09-08 08:17:27 -0300

    FixCommit - adi

--- public/indexCode.mjs
+++ public/indexCode.mjs
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
         const response = await fetch(url + '/adi', opcoes)
         const data = JSON.stringify(await response.json())
-        document.getElementById("amostra").textContent = `${data}`
+        document.getElementById("amostra").textContent = `${data.resultado}`
         get("adi")
         
     });
